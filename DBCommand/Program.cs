c883e37a09cf16e100d1e6b556fe8a10f72d5694
Roko Prpić <roko.prpic@gmail.com>
2023-09-30 11:45:43 +0200

    update OOP exc

---
+++ DBCommand/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DBCommand
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
---
+++ DBConnection/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DBConnection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
